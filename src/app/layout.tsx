ae694b0a480904d0bb95430181bbc82e7dcc1d11
Luke Al-Saba <luke@al-saba.com>
2025-10-11 12:46:28 -0400

    Add onyx-roofing-thumbnail.png and update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -46,7 +46,7 @@ export const metadata: Metadata = {
     siteName: 'Onyx Roofing',
     images: [
       {
-        url: '/onyx-roofing-logo-black.webp',
+        url: '/onyx-roofing-thumbnail.png',
         width: 1200,
         height: 630,
         alt: 'Onyx Roofing - Professional Roofing Services',
