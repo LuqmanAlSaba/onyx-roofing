85980881609434886d31c508da9bbbaeacf9e490
Luke Al-Saba <luke@al-saba.com>
2025-10-21 11:07:51 -0400

    chore: fix font awesome integrity

--- src/app/components/DeferredFontAwesome.tsx
+++ src/app/components/DeferredFontAwesome.tsx
@@ -7,8 +7,8 @@ export default function DeferredFontAwesome() {
     // Load Font Awesome CSS after initial render
     const link = document.createElement("link");
     link.rel = "stylesheet";
-    link.href = "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css";
-    link.integrity = "sha512-bugJ3+Pp4+AoHdbI0W1RCaTq1AdK+/kDKK3S9f+VwIle5Xj2YjGy0sT2BcpQx9L4F0T2tHMWPyClPoHZTLMK7Q==";
+    link.href = "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css";
+    link.integrity = "sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw==";
     link.crossOrigin = "anonymous";
     link.referrerPolicy = "no-referrer";
     document.head.appendChild(link);
